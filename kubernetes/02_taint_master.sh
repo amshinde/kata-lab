51f67cc741a552a585f22a1b8f60a0611d80d812
Archana Shinde <archana.m.shinde@intel.com>
2018-06-28 14:21:40 -0700

    Add lab for kubernetes
    Add scripts to do a kubeadm init and set up flannel as the CNI
    for the cluster.
    
    Signed-off-by: Archana Shinde <archana.m.shinde@intel.com>

---
+++ kubernetes/02_taint_master.sh
@@ -0,0 +1,7 @@
+rm -rf $HOME/.kube
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+master=$(hostname)
+kubectl taint nodes "$master" node-role.kubernetes.io/master:NoSchedule-
